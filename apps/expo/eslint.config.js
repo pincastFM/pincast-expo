598a908e1cb456d2638e600092ba0155eeb6fc18
pincastFM <andrew@pincast.fm>
2025-04-20 20:13:25 -0400

    fix: add ESLint 9.x compatibility

---
+++ apps/expo/eslint.config.js
@@ -0,0 +1,23 @@
+/** @type {import('eslint').Linter.Config} */
+export default {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'vue', 'prettier'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+};
